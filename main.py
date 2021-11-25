701f41301fd0eca08e8449e2f0be7a9b34e54671
yura <yurcin026@gmail.com>
2021-11-25 22:26:09 +0700

    fff

---
+++ main.py
@@ -0,0 +1,39 @@
+import vk_api
+import vk
+import vk_api.bot_longpoll
+from vk_api.utils import get_random_id
+import random
+from config import token
+from data import data
+
+
+def write_msg(chat, msg, chat_id):
+    if msg != False:
+        if chat == 'local':
+            vk.messages.send(user_id = chat_id, message = msg, random_id = get_random_id())
+
+vk_session = vk_api.VkApi(token = token)
+longpoll = vk_api.bot_longpoll.VkBotLongPoll(vk_session, group_id=151231486)
+vk = vk_session.get_api()
+print('Сервер запущен')
+
+
+
+for event in longpoll.listen():
+    print(event.object)
+    obj = event.message
+    if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+        print(f"{obj.from_id}, {obj.text}")
+        if event.from_user:
+            chat_id = obj.from_id
+            msg = obj.text
+            if msg != '':
+                write_msg(chat = 'local', msg = msg, chat_id = chat_id)
+            elif obj.attachments[0]['type'] == 'link':
+                if obj.attachments[0]['link']['title'][-3:] in ['mp3', 'wav', 'ogg']:
+                    write_msg(chat = 'local', msg = random.choice(data), chat_id = chat_id)
+                else:
+                    write_msg(chat = 'local', msg = 'Сука!!! Ты чо скинул', chat_id = chat_id)
+                    
+            else:
+                write_msg(chat = 'local', msg = 'ошибка!', chat_id = chat_id)
